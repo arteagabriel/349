dbb829eda43c40791ceeabac0707d3e8e1c3fdf2
Gabriel Arteaga <gabrielarteaga71@gmail.com>
2016-09-28 17:29:18 -0700

    added merge and quick sort

--- project1/Sorting.java
+++ project1/Sorting.java
@@ -15,7 +15,113 @@ public class Sorting {
         }
     }
 
-    public static void mergeSort(int[] arr, int size) {}
+    public static void mergeSort(int[] arr, int size) {
+        mergeSort(arr, 0, size - 1);
+    }
 
-    public static void quickSort(int[] arr, int size) {}
+    private static void mergeSort(int[] arr, int first, int last) {
+        if (first < last) {
+            int middle = (first + last) / 2;
+            mergeSort(arr, first, middle);
+            mergeSort(arr, middle + 1, last);
+            mergeSortedHalves(arr, first, middle, last);
+        }
+    }
+
+    private static void mergeSortedHalves(int[] arr, int left, int middle, int right) {
+        int[] temp = new int[right - left + 1];
+        int ndx1 = left;
+        int ndx2 = middle + 1;
+        int ndx = 0;
+
+        while (ndx1 < middle + 1 && ndx2 < right + 1) {
+            if (arr[ndx1] < arr[ndx2]) {
+                temp[ndx] = arr[ndx1];
+                ndx1++;
+            } else {
+                temp[ndx] = arr[ndx2];
+                ndx2++;
+            }
+
+            ndx++;
+        }
+
+        if (ndx1 < middle + 1) 
+            for (int i = ndx1; i < middle + 1; i++)
+                temp[ndx++] = arr[i];
+        else
+            for (int i = ndx2; i < right + 1; i++)
+                temp[ndx++] = arr[i];
+
+        int start = left;
+        for (int i = 0; i < temp.length; i++) {
+            arr[start] = temp[i];
+            start++;
+        }
+    }
+
+    public static void quickSort(int[] arr, int size) {
+        quickSort(arr, 0, size - 1);
+    }
+
+    private static void quickSort(int[] arr, int first, int last) {
+        if (first < last) {
+            setPivotToEnd(arr, first, last);
+            int pivotIndex = splitList(arr, first, last);
+            quickSort(arr, first, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, last);
+        }
+    }
+
+    private static void setPivotToEnd(int[] arr, int left, int right) {
+        int center = (left + right) / 2;
+        int temp;
+
+        if (arr[center] < arr[left]) {
+            temp = arr[center];
+            arr[center] = arr[left];
+            arr[left] = temp;
+        }
+
+        if (arr[right] < arr[left]) {
+            temp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = temp;
+        }
+
+        if (arr[center] < arr[right]) {
+            temp = arr[center];
+            arr[center] = arr[right];
+            arr[right] = temp;
+        }
+    }
+
+    private static int splitList(int[] arr, int left, int right) {
+        int indexL = left;
+        int indexR = right - 1;
+        int pivot = arr[right];
+        int temp;
+
+        while (indexL < indexR) {
+            while (arr[indexL] < pivot)
+                indexL++;
+
+            while (indexL < indexR && arr[indexR] > pivot)
+                indexR--;
+
+            if (indexL < indexR) {
+                temp = arr[indexL];
+                arr[indexL] = arr[indexR];
+                arr[indexR] = temp; 
+                indexL++;
+                indexR--;
+            }
+        }
+
+        temp = arr[indexL];
+        arr[indexL] = arr[right];
+        arr[right] = temp;
+
+        return indexL;
+    }
 }
--- project1/SortingTimes.java
+++ project1/SortingTimes.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class SortingTimes {
     public static void main(String[] args) {
-        final int MAX_SIZE = 100;
+        final int MAX_SIZE = 80000;
         int[] arr1 = new int[MAX_SIZE];
         int[] arr2 = new int[MAX_SIZE];
         int[] arr3 = new int[MAX_SIZE];
@@ -12,26 +12,51 @@ public class SortingTimes {
         long startTime;
         long endTime;
         Random rand = new Random();
+        // int[] test = new int[20];
+
+        // for (int i = 0; i < test.length; i++) {
+        //     test[i] = rand.nextInt(test.length);
+        // }
+
+        // Sorting.selectionSort(test, test.length);
+
+        // for (int i : test) {
+        //     System.out.print(i + " ");
+        // }
+        // System.out.println();
 
         System.out.println("Running Times of three sorting algorithms:");
-        for (int n = 5000; n <= 80000; n *= 2) {
+        for (int n = 5000; n <= MAX_SIZE; n *= 2) {
             for (int x = 0; x < 5; x++) {
-                for (int i = 0; i < n; i++)
+                for (int i = 0; i < n; i++) {
                     arr1[i] = rand.nextInt(n);
+                    arr2[i] = rand.nextInt(n);
+                    arr3[i] = rand.nextInt(n);
+                }
 
                 startTime = System.nanoTime();
                 Sorting.selectionSort(arr1, n);
                 endTime = System.nanoTime();
                 ss = (endTime - startTime) / 1000000;
 
-                System.out.println("N=" + n + ": T_ss=" + ss + ", T_ms=" + ms + ", T_qs=" + qs);
-                for (int y = 0; y < MAX_SIZE; y++)
-                    System.out.print(arr1[y] + " ");
-                System.out.println();
+                startTime = System.nanoTime();
+                Sorting.mergeSort(arr2, n);
+                endTime = System.nanoTime();
+                ms = (endTime - startTime) / 1000000;
 
-                for (int z = arr1.length - 10; z < arr1.length; z++)
-                    System.out.print(arr1[z] + " ");
-                System.out.println();
+                startTime = System.nanoTime();
+                Sorting.quickSort(arr3, n);
+                endTime = System.nanoTime();
+                qs = (endTime - startTime) / 1000000;
+
+                System.out.println("N=" + n + ": T_ss=" + ss + ", T_ms=" + ms + ", T_qs=" + qs);
+                // for (int y = 0; y < 10; y++)
+                //     System.out.print(arr3[y] + " ");
+                // System.out.println();
+
+                // for (int z = n - 10; z < n; z++)
+                //     System.out.print(arr3[z] + " ");
+                // System.out.println();
             }
 
             System.out.println();
