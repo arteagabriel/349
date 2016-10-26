f82b7d6101639c8e93fd2a5c7ded2c66662bd560
Gabriel Arteaga <gabrielarteaga71@gmail.com>
2016-10-26 09:51:29 -0700

    changed file input

--- project3/Project31.java
+++ project3/Project31.java
@@ -1,3 +1,10 @@
+/*
+ * Gabriel Arteaga garteaga@calpoly.edu
+ * Alexandra Deany adeany@calpoly.edu
+ * Project 3 part 1
+ * 10/26/2016
+ */
+
 import java.util.Scanner;
 import java.util.Arrays;
 import java.io.*;
@@ -52,7 +59,7 @@ public class Project31 {
     } 
     
     public static void main(String[] args) {
-        String fileName, line = null;
+        File fileName;
         Scanner scan = new Scanner(System.in);
         int n;
         int e1;
@@ -66,58 +73,41 @@ public class Project31 {
         String[] tokens;
 
         System.out.print("Enter file name: ");
-        fileName = scan.next();
+        fileName = new File(scan.next());
         System.out.println();
 
+        scan.close();
         try {
-            FileReader fileReader = new FileReader(fileName);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            scan = new Scanner(fileName);
+            n = scan.nextInt();
 
-            line = bufferedReader.readLine();
-            tokens = line.split(" ");
-            n = Integer.parseInt(tokens[0]);
             a1 = new int[n];
             a2 = new int[n];
             t1 = new int[n-1];
             t2 = new int[n-1];
 
-            line = bufferedReader.readLine();
-            tokens = line.split(" ");
-            e1 = Integer.parseInt(tokens[0]);
-            e2 = Integer.parseInt(tokens[1]);
+            e1 = scan.nextInt();
+            e2 = scan.nextInt();
 
-            line = bufferedReader.readLine();
-            tokens = line.split(" ");
-            x1 = Integer.parseInt(tokens[0]);
-            x2 = Integer.parseInt(tokens[1]);
+            x1 = scan.nextInt();
+            x2 = scan.nextInt();
 
-            line = bufferedReader.readLine();
-            tokens = line.split(" ");
             for (int i = 0; i < n; i++)
-                a1[i] = Integer.parseInt(tokens[i]);
+                a1[i] = scan.nextInt();
 
-            line = bufferedReader.readLine();
-            tokens = line.split(" ");
             for (int i = 0; i < n; i++)
-                a2[i] = Integer.parseInt(tokens[i]);
+                a2[i] = scan.nextInt();
 
-            line = bufferedReader.readLine();
-            tokens = line.split(" ");
             for (int i = 0; i < n - 1; i++)
-                t1[i] = Integer.parseInt(tokens[i]);
+                t1[i] = scan.nextInt();;
 
-            line = bufferedReader.readLine();
-            tokens = line.split(" ");
             for (int i = 0; i < n - 1; i++)
-                t2[i] = Integer.parseInt(tokens[i]);
-
-            bufferedReader.close();         
+                t2[i] = scan.nextInt();
 
             schedule(n, e1, e2, x1, x2, a1, a2, t1, t2);
-        } catch(FileNotFoundException ex) {
-            System.out.println("Unable to open file '" + fileName + "'");                
-        } catch(IOException ex) {
-            System.out.println("Error reading file '" + fileName + "'");
+            scan.close();         
+        } catch (FileNotFoundException ex) {
+            ex.getMessage();
         }
     } 
 }
